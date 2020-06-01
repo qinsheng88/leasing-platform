2f166189b046add7225498b983b4e2e542b7649a
huangziting <huangziting071@163.com>
2020-06-01 19:02:17 +0800

    提交vue template

---
+++ pages/home/home.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: huangziting
+ * @Date: 2020-06-01 18:52:08
+ * @LastEditTime: 2020-06-01 19:01:25
+--> 
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
